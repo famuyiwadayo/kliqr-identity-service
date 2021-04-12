efb150ae5ec82b3a7429b1b7304ebc2bd2b3941c
Famuyiwa Dayo <famuyiwadayodaniel@gmail.com>
2021-04-12 19:34:08 +0100

    added service registry to register self to the service registry and discovery

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
 
 export const config = {
-  DB_URI: process.env.DB_URI,
+  DB_URI: process.env.DB_URI as string,
+  PORT: process.env.PORT as string | number,
+  NAME: process.env.NAME as string,
+  VERSION: process.env.VERSION as string,
+  SERVICE_REGISTRY_URI: process.env.SERVICE_REGISTRY_URI as string,
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,3 @@
-export { default as validateFields } from "./validateFields";
-export { default as setExpiration } from "./setExpiration";
-export { default as getUpdateOptions } from "./getUpdateOptions";
-
 export {
   sendError,
   createError,
